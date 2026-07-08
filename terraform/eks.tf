6e106a5b10edf7dcbc029a89673aa12f48b72474
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-07-08 16:28:00 +0530

    fix: grant GitHub Actions cluster-wide EKS access for Helm installs

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -212,7 +212,6 @@ resource "aws_eks_access_policy_association" "github_actions" {
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
-    type       = "namespace"
-    namespaces = ["food-delivery"]
+    type = "cluster"
   }
 }
